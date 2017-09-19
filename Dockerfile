0076583b7cdaf8de4312054ed94836e2cd5f2bc5
Robin <ybluo@ucdavis.edu>
2017-09-19 13:54:20 -0700

    init push

---
+++ Dockerfile
@@ -0,0 +1,39 @@
+FROM cardcorp/r-pandoc
+
+MAINTAINER "Robin Luo" robin.luo@blackwoodseven.com 
+
+# Install External dependencies 
+RUN apt-get update \
+   && apt-get install -y \
+      libcurl4-openssl-dev \
+      libssl-dev \
+      libsqlite3-dev \
+      libxml2-dev \
+      libmysqlclient-dev\
+      qpdf \
+      vim \
+      git\
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/ 
+
+RUN install2.r --error \
+    devtools \
+    testthat \
+    RMySQL \
+   && rm -rf /tmp/downloaded_packages/ /tmp/*.rds
+
+
+# Install R packages
+RUN R -e "devtools::install_github('robinbing/toy', dependencies = TRUE)"
+
+# Set work dir
+WORKDIR /toy
+ADD . /toy
+
+# Set environmemt variable
+ENV DB.USER robin.luo
+ENV DB.PASSWORD LHC6bf5KvJmX
+ENV CLIENTID 4
+
+# Run
+CMD ["Rscript", "tryprint.R"]
---
+++ tryprint.R
@@ -0,0 +1,31 @@
+mainPrint =
+  #
+  # main function
+  #
+  function(client_id = client.id, user, password) {
+    # check package
+    if(!require(toy)) {
+      library(devtools)
+      install.packages("toy")
+    }
+
+    # fit result
+    real = 1:3
+    predict = real + rnorm(3, 0, 0.1)
+    fit.result = fitStats(real, predict)
+
+    # print Client dbname
+    dbname = printDbname(client_id = client_id, user, password)
+
+    # print result
+    print(paste0('Running client is ', dbname,
+                 '. The final result is %', fit.result$mape * 100, ' mape',
+                 ', %', fit.result$rmse * 100, ' rmse',
+                 ', %', fit.result$mae * 100, ' mae.'))
+  }
+  
+password = Sys.getenv('DB.PASSWORD')
+user = Sys.getenv('DB.USER')
+client_id = Sys.getenv('CLIENTID')
+print(c(client_id, user, password))
+mainPrint(client_id, user, password)
